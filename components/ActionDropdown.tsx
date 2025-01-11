1a14ec04a1c11872616e2d4274815274abb35bdf
jcy97 <sook3427@naver.com>
2025-01-12 02:07:19 +0900

    [feat] 파일 삭제 기능 구현 1. 파일 삭제 로직 구현 2. 테스트 파일 추가

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -22,7 +22,11 @@ import { constructDownloadUrl } from "@/lib/utils";
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { renameFile, updateFileUsers } from "@/lib/actions/file.actions";
+import {
+  deleteFile,
+  renameFile,
+  updateFileUsers,
+} from "@/lib/actions/file.actions";
 import { usePathname } from "next/navigation";
 import { ShareInput, FileDetails } from "@/components/ActionsModalContent";
 
@@ -50,7 +54,8 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
       rename: () =>
         renameFile({ fileId: file.$id, name, extension: file.extension, path }),
       share: () => updateFileUsers({ fileId: file.$id, emails, path }),
-      delete: () => console.log("delete"),
+      delete: () =>
+        deleteFile({ fileId: file.$id, path, bucketFileId: file.bucketFileId }),
     };
     success = await actions[action.value as keyof typeof actions]();
     if (success) closeAllModals();
@@ -90,6 +95,12 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
               onInputChange={setEmails}
               onRemove={handleRemoveUser}
             />
+          )}
+          {value === "delete" && (
+            <p className="delete-confirmation">
+              Are you sure you want to delete {``}
+              <span className="delete-file-name">{file.name}?</span>
+            </p>
           )}
         </DialogHeader>
         {["rename", "delete", "share"].includes(value) && (
